5a7a459ae43147b4eab0e094eb1894b9e8ae47d0
jelena-lucic <lj190268d@student.etf.bg.ac.rs>
2022-08-14 12:06:26 -0700

    Niti cpp api

---
+++ src/Thread.cpp
@@ -0,0 +1,28 @@
+#include "../h/syscall_cpp.hpp"
+
+void Thread::wrapper(void* thread) {
+    if(thread) {
+        ((Thread*)thread)->run();
+    }
+}
+
+Thread::Thread(void (*body)(void *), void *arg) {
+    thread_create(&this->myHandle, body, arg);
+    thread_start(this->myHandle);
+}
+
+Thread::Thread() {
+    thread_create(&this->myHandle, Thread::wrapper, this);
+}
+
+int Thread::start() {
+    return thread_start(this->myHandle);
+}
+
+void Thread::dispatch() {
+    thread_dispatch();
+}
+
+Thread::~Thread() {
+    delete myHandle;
+}
